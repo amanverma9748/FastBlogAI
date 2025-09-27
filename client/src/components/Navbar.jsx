eca051287df7597a61ccb5cca6c9e65bf9e2cb07
unknown <amanverma9748@gmail.com>
2025-09-27 14:35:01 +0530

    Updated client & server configs and removed unused root package files

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -8,7 +8,7 @@ const Navbar = () => {
 
   return (
     <div className='flex justify-between items-center py-5 mx-8 sm:mx-20 xl:mx-32'>
-      <img onClick={() => navigate('/')} src={assets.logo} alt="logo" className='w-32 sm:w-44 cursor-pointer' />
+      <img onClick={() => navigate('/')} src={assets.star_icon} alt="logo" className='w-32 sm:w-4 cursor-pointer' />
       <button onClick={() => navigate('/admin')} className='flex items-center gap-2 rounded-full text-sm cursor-pointer bg-primary text-white px-10 py-2.5 cursor-pointer'>
         {token ? 'Dashboard' : 'Login'}
         <img src={assets.arrow} className='w-3' alt="arrow" />
